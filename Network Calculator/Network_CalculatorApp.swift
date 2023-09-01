2ace0524aef561dc189d7207d95fc41ba3276e28
Tomasz Kubicki <80661109+TKtjpk@users.noreply.github.com>
2023-09-01 14:40:11 +0200

    Initial Commit

---
+++ Network Calculator/Network_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_CalculatorApp.swift
+//  Network Calculator
+//
+//  Created by Tomasz Kubicki on 01/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Network_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
